052d978de409175f6947f4b5d7b73adba70028a9
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-27 02:29:38 -0500

    Add Spinner when form is submit

--- themes/complex-theme/source/js/submit-form.js
+++ themes/complex-theme/source/js/submit-form.js
@@ -9,9 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (emailTest.test(emailInput.value)) {
       grecaptcha.execute();
-      setTimeout(() => {
-        closeNotification();
-      }, 10000);
     }
 
     return false;
@@ -33,19 +30,28 @@ function onSubmit() {
   const form = document.getElementById("suscription-form");
   const emailInput = document.getElementById("email");
   const sendBtn = document.getElementById("send");
+  const loader = document.getElementById("send-loader");
 
   form.submit();
+  emailInput.readOnly = true;
+  sendBtn.readOnly = true;
+  loader.hidden = false;
 
   const iframe = document.getElementById("suscription-iframe");
   iframe.addEventListener("load", () => {
+    loader.hidden = true;
+
     // Open Notification Popup
     openNotification();
+    setTimeout(() => closeNotification(), 5000);
 
     // Reset Recaptcha
     grecaptcha.reset();
 
     // Reset Form to accept news suscriptions
+    emailInput.readOnly = false;
     emailInput.value = "";
+    sendBtn.readOnly = false;
     sendBtn.disabled = true;
   });
 }
