17eccd1f522cb31c9bcfc479a76a57734f302947
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-27 02:14:40 -0500

    adding notification popup

--- themes/complex-theme/source/js/submit-form.js
+++ themes/complex-theme/source/js/submit-form.js
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (emailTest.test(emailInput.value)) {
       grecaptcha.execute();
+      setTimeout(() => {
+        closeNotification();
+      }, 10000);
     }
 
     return false;
@@ -17,22 +20,26 @@ document.addEventListener("DOMContentLoaded", () => {
   emailInput.addEventListener("input", () => {
     sendBtn.disabled = !emailTest.test(emailInput.value);
   });
+
+  const notificationCloseBtn = document.getElementById(
+    "suscription-notification-close"
+  );
+  notificationCloseBtn.addEventListener("click", () => {
+    closeNotification();
+  });
 });
 
 function onSubmit() {
   const form = document.getElementById("suscription-form");
+  const emailInput = document.getElementById("email");
+  const sendBtn = document.getElementById("send");
+
   form.submit();
 
   const iframe = document.getElementById("suscription-iframe");
   iframe.addEventListener("load", () => {
-    // Open message
-    /**
-     * ¡Gracias por suscribirte!
-     *   En breve recibirás un correo con instrucciones para la
-     *   activación de tu cuenta.
-     */
-
-    alert("¡Gracias por suscribirte!");
+    // Open Notification Popup
+    openNotification();
 
     // Reset Recaptcha
     grecaptcha.reset();
@@ -41,4 +48,20 @@ function onSubmit() {
     emailInput.value = "";
     sendBtn.disabled = true;
   });
+}
+
+function openNotification() {
+  const popUp = document.getElementById("suscription-notification");
+  popUp.hidden = false;
+  setTimeout(() => popUp.classList.add("fade-in"));
+}
+
+function closeNotification() {
+  const popUp = document.getElementById("suscription-notification");
+  popUp.classList.remove("fade-in");
+  setTimeout(() => {
+    if (popUp.classList.contains("fade-in")) {
+      popUp.hidden = true;
+    }
+  }, 300);
 }
